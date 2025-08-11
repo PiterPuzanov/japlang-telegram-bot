4b1f8c089623798fa8dd3bdc6f9f0c4c60a06e99
Puzanov Petr <peta12321@gmail.com>
2025-08-11 09:20:19 +0300

    init commit

---
+++ src/main/java/com/lang/japlang_telegram_bot/JaplangTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.lang.japlang_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaplangTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaplangTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lang/japlang_telegram_bot/JaplangTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lang.japlang_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaplangTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
